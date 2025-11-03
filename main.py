5790fc4f10a1db91459ff8fcf30558df870be469
hagay kohelet <hagay2506@gmail.com>
2025-11-03 18:05:10 +0200

    robots

--- main.py
+++ main.py
@@ -16,30 +16,29 @@ if __name__ == "__main__":
         3.where
         4.exit
     -------------------------""")
-        try:
-            cli = input("choose some num of command: ")
-            match cli:
-                     case "1":
-                         stri = input("please enter some str")
-                         dora.say(stri)
-                         gideon.say(stri)
 
-                     case "2":
-                         location1 = int(input("enter your x point: "))
-                         location2 = int(input("enter your y point: "))
-                         locatins = (location1,location2)
-                         dora.move(location1,location2)
-                         gideon.move(location1,location2)
+        cli = input("choose some num of command: ")
+        match cli:
+                 case "1":
+                     stri = input("please enter some str")
+                     dora.say(stri)
+                     gideon.say(stri)
 
-
-                     case "3":
-                         dora.location()
-                         gideon.location()
+                 case "2":
+                     location1 = int(input("enter your x point: "))
+                     location2 = int(input("enter your y point: "))
+                     locatins = (location1,location2)
+                     dora.move(location1,location2)
+                     gideon.move(location1,location2)
 
 
+                 case "3":
+                     dora.location()
+                     gideon.location()
 
-                     case "4":
-                        exit()
 
-        except ValueError:
-            print("illegal input")
+
+                 case "4":
+                    print("good bye")
+                    break
+
